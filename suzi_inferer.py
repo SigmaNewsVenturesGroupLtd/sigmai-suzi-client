8f73fe76c6c0cd5722529f12d6072eb7e060ea9e
Martin Boyanov <mboyanov@sigm.ai>
2018-10-15 15:55:42 +0300

    add multi processing and higher batch size

--- suzi_inferer.py
+++ suzi_inferer.py
@@ -5,8 +5,8 @@ inferer = Inferer(MODEL_PATH, VOCAB_PATH)
 
 
 def tag_articles(articles):
-    return inferer.tag_articles(articles)
+    return inferer.tag_articles(articles, multi_processing=True, batch_size=10000)
 
 
 def score_articles(articles):
-    return inferer.score_articles(articles)
+    return inferer.score_articles(articles, multi_processing=True, batch_size=10000)
